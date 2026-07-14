0e923518ce46280ed656fa00638f388c59ceb3e2
sanjai075 <sanjaiprakash075@gmail.com>
2026-07-14 20:59:38 +0530

    chore: ignore ESLint warnings during production builds

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
